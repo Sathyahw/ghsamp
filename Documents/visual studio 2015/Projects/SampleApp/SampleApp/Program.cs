24d3a17e853a11cc1685ae6b14f84095f566223e
H156368 <H156368@IE22LT8VWL6C2>
2016-09-16 10:28:21 +0530

    d

--- Documents/visual studio 2015/Projects/SampleApp/SampleApp/Program.cs
+++ Documents/visual studio 2015/Projects/SampleApp/SampleApp/Program.cs
@@ -10,7 +10,7 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World config !!!");
+            Console.WriteLine("Hello World config 123!!!");
             Console.ReadKey();
         }
     }
